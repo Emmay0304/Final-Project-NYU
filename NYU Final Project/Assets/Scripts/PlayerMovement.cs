94012c1427fd64d1a5b31b77c28c9c53be1408bf
MirandaNi0802 <mirandalyni0802@gmail.com>
2023-07-18 15:28:40 -0400

    BGM+Ground Collider

--- NYU Final Project/Assets/Scripts/PlayerMovement.cs
+++ NYU Final Project/Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,11 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
 
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +20,17 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        controlle
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
     }
 }
