e99ce772b0f76663813e801556367b64a2d215cc
soyaoh <139798714+soyaoh@users.noreply.github.com>
2023-07-18 18:38:46 -0700

    Added character walk animation

--- NYU Final Project/Assets/Scripts/PlayerMovement.cs
+++ NYU Final Project/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
+    public Animator animator;
 
     public float runSpeed = 40f;
 
@@ -21,6 +22,8 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
         if (Input.GetButtonDown("Jump"))
         {
